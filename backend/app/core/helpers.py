7e319370ce69b171eec5823c4d889e2921365dd5
dejvinczi <dejvinczi.main@gmail.com>
2024-04-02 19:11:07 +0200

    Update helpers.py - rename correct way func name and description

--- backend/app/core/helpers.py
+++ backend/app/core/helpers.py
@@ -5,7 +5,7 @@ Core helpers.
 import uuid
 
 
-def generate_room_link():
+def generate_room_id():
     # TODO - Delete or save
-    """Generate unique based on uuid4 alphanumeric room link."""
+    """Generate unique based on uuid4 alphanumeric room id."""
     return str(uuid.uuid4()).replace("-", "")
