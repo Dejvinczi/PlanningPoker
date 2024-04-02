ebfcb6a46806f51fbc493c35c78b0565d8e1ccaf
dejvinczi <dejvinczi.main@gmail.com>
2024-04-02 19:11:09 +0200

    Update JoinRoomSerializer (validation state)

--- backend/app/core/api/serializers.py
+++ backend/app/core/api/serializers.py
@@ -39,27 +39,25 @@ class JoinRoomSerializer(serializers.ModelSerializer):
         fields = ["id", "room", "password", "voter"]
         read_only = ["id"]
 
-    def create(self, validated_data):
-        """Return instance od Vote model."""
-        password = validated_data.pop("password", "")
-        room_id = validated_data.pop("room")
-        voter = validated_data.get("voter")
-
-        # Check if the room exists
+    def validate_room(self, value):
+        """Validate if the room exists."""
         try:
-            room = Room.objects.get(id=room_id)
-            validated_data.update({"room": room})
+            return Room.objects.get(id=value)
         except (Room.DoesNotExist, ValidationError):
             raise serializers.ValidationError({"room": _("Room doesn't exists.")})
 
-        # Check if room password correct
+    def validate(self, data):
+        """Object level validation."""
+        room = data.get("room")
+        password = data.pop("password", "")
+        voter = data.get("voter")
+
         if not room.check_password(password):
             raise serializers.ValidationError({"password": _("Invalid password.")})
 
-        # Check if the voter already exists in the room
         if Vote.objects.filter(room=room, voter=voter).exists():
             raise serializers.ValidationError(
                 {"voter": _("This name is already reserved by another voter.")}
             )
 
-        return super().create(validated_data)
+        return data
